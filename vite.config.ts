835fca3b2a1ab260562ea8b221d4aa08524c8557
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-07-09 22:10:11 +0200

    refactor: add base name to vite congig

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rs-react-app/',
+  base: '/rs-react-app-2025/',
   plugins: [react()],
 });
