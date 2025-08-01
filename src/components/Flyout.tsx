4455067e3c39e8f2191d8d70d95ff2acb9b75459
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-08-01 21:44:37 +0200

    fix: fix flyout styles to see pagination

--- src/components/Flyout.tsx
+++ src/components/Flyout.tsx
@@ -12,9 +12,9 @@ const Flyout: React.FC<FlyoutProps> = ({ count, onUnselectAll, children }) => {
 
   return (
     <div
-      className={`fixed bottom-0 left-0 w-full flex justify-center pointer-events-none transition-transform duration-500 ease-in will-change-transform ${animateClass}`}
+      className={`fixed bottom-20 left-0 w-full flex justify-center pointer-events-none transition-transform duration-500 ease-in will-change-transform ${animateClass}`}
     >
-      <div className="flex items-center gap-4 bg-white dark:bg-gray-900 shadow-lg rounded-t-2xl px-6 py-4 mb-0 border-4 border-b-0 border-blue-400 dark:border-gray-700  pointer-events-auto transition-all">
+      <div className="flex items-center gap-4 bg-white dark:bg-gray-900 shadow-lg rounded-2xl px-10 py-10 mb-0 border-4 border-blue-400 dark:border-gray-700  pointer-events-auto transition-all">
         <span className="font-semibold text-gray-900 dark:text-gray-100 text-lg">
           {count > 0 && `${count} ${count === 1 ? 'item' : 'items'} selected`}
           {count === 0 && 'Bye!'}
