581c01637912175b226f80da8e3d0b6f754b9ab8
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-08-01 22:00:20 +0200

    fix: fix flyout width

--- src/components/Flyout.tsx
+++ src/components/Flyout.tsx
@@ -15,15 +15,17 @@ const Flyout: React.FC<FlyoutProps> = ({ count, onUnselectAll, children }) => {
     <div
       className={`fixed left-0 w-full flex justify-center pointer-events-none transition-all duration-500 ease-in will-change-transform ${animateClass}`}
     >
-      <div className="flex items-center gap-4 bg-white dark:bg-gray-900 shadow-lg rounded-2xl px-10 py-10 mb-0 border-4 border-blue-400 dark:border-gray-700  pointer-events-auto transition-all">
-        <span className="font-semibold text-gray-900 dark:text-gray-100 text-lg">
+      <div className="flex items-center justify-around gap-2 bg-white dark:bg-gray-900 shadow-lg rounded-2xl px-10 py-10 mb-0 border-4 border-blue-400 dark:border-gray-700 w-[510px] pointer-events-auto transition-all">
+        <span className="font-semibold text-gray-900 dark:text-gray-100 text-lg flex-1">
           {count > 0 && `${count} ${count === 1 ? 'item' : 'items'} selected`}
           {count === 0 && 'Bye!'}
         </span>
-        <Button onClick={onUnselectAll} variant="secondary">
-          Unselect all
-        </Button>
-        {children}
+        <>
+          <Button onClick={onUnselectAll} variant="secondary">
+            Unselect all
+          </Button>
+          {children}
+        </>
       </div>
     </div>
   );
