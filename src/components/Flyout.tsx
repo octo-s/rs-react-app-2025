7d9e81103ce8a9ab60a3bdad5ef05ad9fbcf33e6
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-08-01 21:51:14 +0200

    fix: fix flyout animation

--- src/components/Flyout.tsx
+++ src/components/Flyout.tsx
@@ -8,11 +8,12 @@ type FlyoutProps = {
 };
 
 const Flyout: React.FC<FlyoutProps> = ({ count, onUnselectAll, children }) => {
-  const animateClass = count > 0 ? 'translate-y-0' : 'translate-y-full';
+  const animateClass =
+    count > 0 ? 'translate-y-0 bottom-20' : 'translate-y-full bottom-0';
 
   return (
     <div
-      className={`fixed bottom-20 left-0 w-full flex justify-center pointer-events-none transition-transform duration-500 ease-in will-change-transform ${animateClass}`}
+      className={`fixed left-0 w-full flex justify-center pointer-events-none transition-all duration-500 ease-in will-change-transform ${animateClass}`}
     >
       <div className="flex items-center gap-4 bg-white dark:bg-gray-900 shadow-lg rounded-2xl px-10 py-10 mb-0 border-4 border-blue-400 dark:border-gray-700  pointer-events-auto transition-all">
         <span className="font-semibold text-gray-900 dark:text-gray-100 text-lg">
