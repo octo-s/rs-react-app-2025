b80bbe236711d9a8918c457ad96bd222790bab8d
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-08-03 16:05:10 +0200

    fix: fix elements download

--- src/__utils__/generateDataUrl.ts
+++
@@ -1,7 +0,0 @@
-import type { Character } from '../__types__/characters.ts';
-import { arrayToCSV } from './arrayToCSV.ts';
-
-export function generateDataUrl(items: Character[]): string {
-  const csv = arrayToCSV(items);
-  return `data:text/csv;charset=utf-8,${encodeURIComponent(csv)}`;
-}
--- src/components/SelectedCharacters.tsx
+++ src/components/SelectedCharacters.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { generateDataUrl } from '../__utils__/generateDataUrl.ts';
+import React, { useEffect, useRef, useState } from 'react';
 import { unselectAll } from '../entities/selected/selectedSlice.ts';
 import { useDispatch, useSelector } from 'react-redux';
 import type { RootState } from '../store';
 import Flyout from './Flyout.tsx';
+import { arrayToCSV } from '../__utils__/arrayToCSV.ts';
 
 const SelectedCharacters: React.FC = () => {
   const [show, setShow] = useState(false);
+  const linkRef = useRef<HTMLAnchorElement | null>(null);
+
   const dispatch = useDispatch();
   const selectedCharacters = useSelector(
     (state: RootState) => state.selected.selectedCharacters
@@ -22,18 +24,32 @@ const SelectedCharacters: React.FC = () => {
     }
   }, [selectedCharacters.length]);
 
-  if (!show && count === 0) return null;
-
   const onUnselectAll = () => {
     dispatch(unselectAll());
   };
 
+  function handleClick() {
+    const csv = arrayToCSV(selectedCharacters);
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+
+    if (linkRef.current) {
+      linkRef.current.href = url;
+      linkRef.current.download = `${selectedCharacters.length}_items.csv`;
+
+      setTimeout(() => URL.revokeObjectURL(url), 2000);
+    }
+  }
+
+  if (!show && count === 0) return null;
+
   return (
     <Flyout onUnselectAll={onUnselectAll} count={count}>
       <a
+        onClick={handleClick}
+        ref={linkRef}
         className="px-6 py-2 rounded font-semibold shadow outline-none border-none transition bg-blue-600 text-white hover:text-white dark:bg-red-900"
-        href={generateDataUrl(selectedCharacters)}
-        download={`${selectedCharacters.length}_items.csv`}
+        href=""
       >
         Download
       </a>
